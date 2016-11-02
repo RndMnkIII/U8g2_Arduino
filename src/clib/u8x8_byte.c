f147c776abdfce72bb0fb0c97b00ef14587d74fd
RndMnkIII <fumofz@yahoo.es>
2016-11-02 19:12:16 +0100

    Update u8x8_byte.c
    Replaced call to gpio write by direct write to MK20DX256VLH7 port D (data port hardwired to this Teensy 3.1/3.2 pins: 2,14,7,8,6,20,21,5.

--- src/clib/u8x8_byte.c
+++ src/clib/u8x8_byte.c
@@ -35,7 +35,38 @@
 */
 
 #include "u8x8.h"
+// General-Purpose Input/Output (GPIO) Teensy 3.1/3.2 CPU MK20DX256VLH7 Cortex-M4
 
+#define GPIOA_PDOR		(*(volatile uint32_t *)0x400FF000) // Port Data Output Register
+#define GPIOA_PSOR		(*(volatile uint32_t *)0x400FF004) // Port Set Output Register
+#define GPIOA_PCOR		(*(volatile uint32_t *)0x400FF008) // Port Clear Output Register
+#define GPIOA_PTOR		(*(volatile uint32_t *)0x400FF00C) // Port Toggle Output Register
+#define GPIOA_PDIR		(*(volatile uint32_t *)0x400FF010) // Port Data Input Register
+#define GPIOA_PDDR		(*(volatile uint32_t *)0x400FF014) // Port Data Direction Register
+#define GPIOB_PDOR		(*(volatile uint32_t *)0x400FF040) // Port Data Output Register
+#define GPIOB_PSOR		(*(volatile uint32_t *)0x400FF044) // Port Set Output Register
+#define GPIOB_PCOR		(*(volatile uint32_t *)0x400FF048) // Port Clear Output Register
+#define GPIOB_PTOR		(*(volatile uint32_t *)0x400FF04C) // Port Toggle Output Register
+#define GPIOB_PDIR		(*(volatile uint32_t *)0x400FF050) // Port Data Input Register
+#define GPIOB_PDDR		(*(volatile uint32_t *)0x400FF054) // Port Data Direction Register
+#define GPIOC_PDOR		(*(volatile uint32_t *)0x400FF080) // Port Data Output Register
+#define GPIOC_PSOR		(*(volatile uint32_t *)0x400FF084) // Port Set Output Register
+#define GPIOC_PCOR		(*(volatile uint32_t *)0x400FF088) // Port Clear Output Register
+#define GPIOC_PTOR		(*(volatile uint32_t *)0x400FF08C) // Port Toggle Output Register
+#define GPIOC_PDIR		(*(volatile uint32_t *)0x400FF090) // Port Data Input Register
+#define GPIOC_PDDR		(*(volatile uint32_t *)0x400FF094) // Port Data Direction Register
+#define GPIOD_PDOR		(*(volatile uint32_t *)0x400FF0C0) // Port Data Output Register
+#define GPIOD_PSOR		(*(volatile uint32_t *)0x400FF0C4) // Port Set Output Register
+#define GPIOD_PCOR		(*(volatile uint32_t *)0x400FF0C8) // Port Clear Output Register
+#define GPIOD_PTOR		(*(volatile uint32_t *)0x400FF0CC) // Port Toggle Output Register
+#define GPIOD_PDIR		(*(volatile uint32_t *)0x400FF0D0) // Port Data Input Register
+#define GPIOD_PDDR		(*(volatile uint32_t *)0x400FF0D4) // Port Data Direction Register
+#define GPIOE_PDOR		(*(volatile uint32_t *)0x400FF100) // Port Data Output Register
+#define GPIOE_PSOR		(*(volatile uint32_t *)0x400FF104) // Port Set Output Register
+#define GPIOE_PCOR		(*(volatile uint32_t *)0x400FF108) // Port Clear Output Register
+#define GPIOE_PTOR		(*(volatile uint32_t *)0x400FF10C) // Port Toggle Output Register
+#define GPIOE_PDIR		(*(volatile uint32_t *)0x400FF110) // Port Data Input Register
+#define GPIOE_PDDR		(*(volatile uint32_t *)0x400FF114) // Port Data Direction Register
 uint8_t u8x8_byte_SetDC(u8x8_t *u8x8, uint8_t dc)
 {
   return u8x8->byte_cb(u8x8, U8X8_MSG_BYTE_SET_DC, dc, NULL);
@@ -206,13 +237,14 @@ uint8_t u8x8_byte_8bit_8080mode(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void
       while( arg_int > 0 )
       {
 	b = *data;
+	GPIOD_PDOR = b;
 	data++;
 	arg_int--;
-	for( i = U8X8_MSG_GPIO_D0; i <= U8X8_MSG_GPIO_D7; i++ )
-	{
-	  u8x8_gpio_call(u8x8, i, b&1);
-	  b >>= 1;
-	}    
+	//for( i = U8X8_MSG_GPIO_D0; i <= U8X8_MSG_GPIO_D7; i++ )
+	//{
+	//  u8x8_gpio_call(u8x8, i, b&1);
+	//  b >>= 1;
+	//}    
 	
 	u8x8_gpio_Delay(u8x8, U8X8_MSG_DELAY_NANO, u8x8->display_info->data_setup_time_ns);
 	u8x8_gpio_call(u8x8, U8X8_MSG_GPIO_E, 0);
